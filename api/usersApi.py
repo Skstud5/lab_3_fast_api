c69debf9d1c394c75ae5c6537a72d55e02c7f2d9
Skstud5 <skripchenko@rb.asu.ru>
2024-01-07 00:41:20 +0700

    fix work create_user

--- api/usersApi.py
+++ api/usersApi.py
@@ -6,7 +6,7 @@ from data_base.data_base import get_session
 from fastapi import HTTPException
 from passlib.hash import pbkdf2_sha256
 from sqlalchemy import select
-
+from sqlalchemy.ext.asyncio import AsyncSession
 
 users_router = APIRouter(tags=[Tags.users], prefix='/api/users')
 
@@ -31,14 +31,17 @@ async def get_user(user_id: int, data_base: Session = Depends(get_session)):
 
 
 @users_router.post("/", response_model=User)
-async def create_user(cr_user: User, data_base: Session = Depends(get_session)):
+async def create_user(cr_user: User, data_base: AsyncSession = Depends(get_session)):
     try:
         user = UserEntity(name=cr_user.name, hash_password=password_encryption(cr_user.name))
         if user is None:
             raise HTTPException(status_code=404, detail="Объект не определен")
-        data_base.add(user)
-        data_base.commit()
-        data_base.refresh(user)
-        return user
+
+        async with data_base.begin():
+            data_base.add(user)
+            await data_base.commit()
+            await data_base.refresh(user)
+        result = User(id=user.id, name=user.name, hash_password=user.hash_password)
+        return result
     except Exception as exc:
         raise HTTPException(status_code=500, detail=f"Ошибка при добавлении {cr_user}")
