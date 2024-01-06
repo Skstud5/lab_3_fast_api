f150b873bcf3041ac5408ac2159877956e17f924
Skstud5 <skripchenko@rb.asu.ru>
2024-01-06 21:01:08 +0700

    connection and set config

---
+++ config/config.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+import os
+
+
+class Settings:
+    app_name: str = "New API"
+    admin_email: str = "d1225ssd@rb.asu.ru"
+    DATABASE_URL_SYNC: str = "postgresql://adminMovie:5806719824@localhost:5455/movies"
+    DATABASE_URL_ASYNC: str = "postgresql+asyncpg://adminMovie:5806719824@localhost:5455/movies"
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_DB: str
+
+
+load_dotenv()
+settings = Settings()
+
+settings.POSTGRES_USER = os.environ.get('POSTGRES_USER')
+settings.POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+settings.POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+settings.POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
+settings.POSTGRES_DB = os.environ.get('POSTGRES_DB')
+
+settings.DATABASE_URL_SYNC = f"postgresql:" \
+                              f"//{settings.POSTGRES_USER}:" \
+                              f"{settings.POSTGRES_PASSWORD}" \
+                              f"@{settings.POSTGRES_HOST}:" \
+                              f"{settings.POSTGRES_PORT}" \
+                              f"/{settings.POSTGRES_DB}"
+
+settings.DATABASE_URL_ASYNC = f"postgresql+asyncpg:" \
+                              f"//{settings.POSTGRES_USER}:" \
+                              f"{settings.POSTGRES_PASSWORD}" \
+                              f"@{settings.POSTGRES_HOST}:" \
+                              f"{settings.POSTGRES_PORT}" \
+                              f"/{settings.POSTGRES_DB}"
--- data_base/data_base.py
+++ data_base/data_base.py
@@ -1,3 +1,24 @@
-from sqlalchemy import create_engine
-DATABASE_URI = "postgresql://postgres:9782157241@localhost:5455/movies"
-engine = create_engine(DATABASE_URI)
+import asyncio
+from config.config import settings
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import create_async_engine
+
+DATABASE_URI_ASYNC = "postgresql+asyncpg://adminMovie:5806719824@localhost:5455/movies"
+
+URI_SYNC = settings.DATABASE_URL_SYNC
+URI_ASYNC = settings.DATABASE_URL_ASYNC
+
+print(URI_SYNC)
+print(URI_ASYNC)
+print(DATABASE_URI_ASYNC)
+
+engine = create_async_engine(URI_ASYNC, echo=True)
+
+
+async def connect_to_database():
+    async with engine.connect() as connection:
+        result = await connection.execute(text("select version()"))
+        print(f"answer = {result.all()}")
+
+
+asyncio.run(connect_to_database())
